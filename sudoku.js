d42486f7a36f3e949df19faff90b6b0b73ac14c7
Nikita-Petrov0 <petrovn.46.22@gmail.com>
2025-02-14 10:58:43 +0300

    rel1

--- sudoku.js
+++ sudoku.js
@@ -1,10 +1,26 @@
-function read() {
-  /**
-   * Прочесть файл puzzles.txt в кодировке 'utf-8' и вернуть эти данные из функции
-   */
+
+const fs = require('fs')
+const {EOL} = require('os')
+
+function read(variant) {
+  const board = fs.readFileSync('./puzzles.txt', 'utf8')
+  const result = board.split(EOL)
+  const puzzles = []
+  for (let item of result) {
+    const newArr = []
+    for (let i = 0; i < 9; i++) {
+      newArr.push(item.slice(i * 9, (i + 1) * 9).split(''))
+    }
+    puzzles.push(newArr)
+    
+  }
+  return puzzles[variant+1]
+    
 }
+console.log(read(2))
 
 function solve() {
+  // 
   /**
    * Принимает игровое поле в том формате, в котором его вернули из функции read.
    * Возвращает игровое поле после попытки его решить.
