59e3b7341ef52bd2858bbd2ce79cbd321744575d
DoKTopBaHTyc <denisoid93@gmail.com>
2025-02-14 12:33:03 +0300

    table release 0

--- sudoku.js
+++ sudoku.js
@@ -1,26 +1,23 @@
-
-const fs = require('fs')
-const {EOL} = require('os')
+const { log, table } = require('console');
+const fs = require('fs');
+const { EOL } = require('os');
 
 function read(variant) {
-  const board = fs.readFileSync('./puzzles.txt', 'utf8')
-  const result = board.split(EOL)
-  const puzzles = []
-  for (let item of result) {
-    const newArr = []
+  const board = fs.readFileSync('./puzzles.txt', 'utf8');
+  const result = board.split(EOL);
+  const puzzles = [];
+  for (const item of result) {
+    const newArr = [];
     for (let i = 0; i < 9; i++) {
-      newArr.push(item.slice(i * 9, (i + 1) * 9).split(''))
+      newArr.push(item.slice(i * 9, (i + 1) * 9).split(''));
     }
-    puzzles.push(newArr)
-    
+    puzzles.push(newArr);
   }
-  return puzzles[variant+1]
-    
+  return puzzles[variant + 1];
 }
-console.log(read(2))
 
 function solve() {
-  // 
+  //
   /**
    * Принимает игровое поле в том формате, в котором его вернули из функции read.
    * Возвращает игровое поле после попытки его решить.
@@ -34,10 +31,19 @@ function isSolved() {
    */
 }
 
-function prettyBoard() {
-  /**
-   * Принимает игровое поле в том формате, в котором его вернули из функции solve.
-   * Выводит в консоль/терминал судоку.
-   * Подумай, как симпатичнее его вывести.
-   */
+const result = read(-1);
+
+function prettyBoard(result) {
+  const tableResult = result.join('').split(',').join('');
+  let newStr = '';
+  for (let i = 0; i < tableResult.length; i++) {
+    if (i % 9 === 0) {
+      newStr += `\n ||`;
+    }
+    newStr += `${tableResult[i]}|`;
+  }
+  return newStr;
 }
+
+console.log(prettyBoard(result));
+
