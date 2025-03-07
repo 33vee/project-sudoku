089fdc1eee44dd22e614f4c2c5aabd035b110fcd
Tihon <Ltihonol@yandex.ru>
2025-03-07 12:07:06 +0300

    read release

--- sudoku.js
+++ sudoku.js
@@ -1,27 +1,19 @@
-const fs = require('fs')
-const { EOL } = require('os')
-const { markAsUntransferable } = require('worker_threads')
-
+const fs = require('fs');
+const { EOL } = require('os');
+const { markAsUntransferable } = require('worker_threads');
 
 function read(num) {
   /**
    * Прочесть файл puzzles.txt в кодировке 'utf-8' и вернуть эти данные из функции
    */
-  const allBoard = fs.readFileSync('./puzzles.txt', 'utf-8').split(EOL)[num].split('')
-  let res = []
-
-let size = 9
-
-for (let i = 0; i < allBoard.length; i+= size) {
-  res.push(allBoard.slice(i, i + size))
-  
+  const allBoard = fs.readFileSync('./puzzles.txt', 'utf-8').split(EOL)[num].split('');
+  const res = [];
+  for (let i = 0; i < allBoard.length; i += 9) {
+    res.push(allBoard.slice(i, i + 9));
+  }
+  return res;
 }
-
-return res
-
-}
-console.log(read(0));
-
+console.log(read(14));
 
 function solve() {
   /**
