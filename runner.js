5e96897c2e91f31a7b736eea6c5e3ee26d74f231
7Eighty <igrovoi.ac@yandex.ru>
2025-05-16 15:06:51 +0300

    sudoku/feat/runner

--- runner.js
+++ runner.js
@@ -1,1 +1,22 @@
 const {read, solve, isSolved, prettyBoard} = require('./sudoku')
+function run(num) {
+  try {
+    const board = read(num);
+    console.log('Исходная судоку:');
+    console.log(prettyBoard(board));
+
+const solvedBoard = solve(board);
+
+console.log('\nРешённая судоку:');
+console.log(prettyBoard(solvedBoard));
+
+console.log(
+  '\nСтатус:',
+  isSolved(solvedBoard) ? '✅ Решено!' : '❌ Не удалось решить.',
+);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+run(Math.floor(Math.random() * 15) + 1)
