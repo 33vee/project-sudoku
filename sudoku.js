0dca0004c513a7f900da28aab0b3410da4522a76
Eonian-boop <eoniancake@gmail.com>
2025-06-27 15:31:29 +0300

    prettyBoardfunc

--- sudoku.js
+++ sudoku.js
@@ -1,6 +1,6 @@
 function read() {
   /**
-   * Прочесть файл puzzles.txt в кодировке 'utf-8' и вернуть эти данные из функции
+   * Прочесть файл puzzles.txt в кодировке 'utf08' и вернуть эти данные из функции
    */
 }
 
@@ -11,17 +11,53 @@ function solve() {
    */
 }
 
-function isSolved() {
-  /**
-   * Принимает игровое поле в том формате, в котором его вернули из функции solve.
-   * Возвращает булевое значение — решено это игровое поле или нет.
-   */
+const arr = [
+  [1, 0, 5, 8, 0, 2, 0, 0, 0],
+  [0, 9, 0, 0, 7, 6, 4, 0, 5],
+  [2, 0, 0, 4, 0, 0, 8, 1, 9],
+  [0, 1, 9, 0, 0, 7, 3, 0, 6],
+  [7, 6, 2, 0, 8, 3, 0, 9, 0],
+  [0, 0, 0, 0, 6, 1, 0, 5, 0],
+  [0, 0, 7, 6, 0, 0, 0, 3, 0],
+  [4, 3, 0, 0, 2, 0, 5, 0, 0],
+  [6, 0, 0, 3, 0, 8, 9, 0, 0],
+];
+function prettyBoard(solve) {
+  const flatArr = solve.flat();
+  for (let i = 2; i < flatArr.length; i += 3) {
+    if (
+      i !== 8 &&
+      i !== 17 &&
+      i !== 26 &&
+      i !== 35 &&
+      i !== 44 &&
+      i !== 53 &&
+      i !== 62 &&
+      i !== 71 &&
+      i !== 80
+    )
+      flatArr[i] += ' |';
+  }
+  for (let i = 8; i < flatArr.length; i += 9) {
+    flatArr[i] += '\n';
+  }
+  let str = ' ';
+  const newNewArr = flatArr
+    .map((el) => `${el} `)
+    .join('')
+    .trim();
+  const newStr = str + newNewArr;
+  console.log(newStr.slice(0, 24).trim());
+  console.log(newStr.slice(24, 47).trim());
+  console.log(newStr.slice(47, 70).trim());
+  console.log('---------------------');
+  console.log(newStr.slice(70, 93).trim());
+  console.log(newStr.slice(93, 116).trim());
+  console.log(newStr.slice(116, 139).trim());
+  console.log('---------------------');
+  console.log(newStr.slice(139, 162).trim());
+  console.log(newStr.slice(162, 185).trim());
+  console.log(newStr.slice(185, 208).trim());
 }
 
-function prettyBoard() {
-  /**
-   * Принимает игровое поле в том формате, в котором его вернули из функции solve.
-   * Выводит в консоль/терминал судоку.
-   * Подумай, как симпатичнее его вывести.
-   */
-}
+prettyBoard(arr);
