41677214bdf9d20029328dd94b10e857156d6d63
Evgeny-Den <zdenisov99@yandex.ru>
2025-08-29 15:28:49 +0300

    Сделал валидацию (наверное)

--- sudoku.js
+++ sudoku.js
@@ -1,5 +1,16 @@
-const randomizer = require('./randomizer')
+const sudoArr = [
+  '1-58-2---',
+  '-9--764-5',
+  '2--4--819]',
+  '-19--73-6',
+  '762-83-9-',
+  '----61-5-',
+  '--76---3-',
+  '43--2-5-1',
+  '6--3-89--',
+];
 
+const board = sudoArr.map((el) => el.split(''));
 
 function read() {
   /**
@@ -7,21 +18,34 @@ function read() {
    */
 }
 
-function solve() {
+function solve(arr) {
   /**
    * Принимает игровое поле в том формате, в котором его вернули из функции read.
    * Возвращает игровое поле после попытки его решить.
    */
 }
 
-function isSolved(resultSolve) {
+function isSolved(row, col, num) {
   /**
    * Принимает игровое поле в том формате, в котором его вернули из функции solve.
    * Возвращает булевое значение — решено это игровое поле или нет.
    */
-  if (solveArr)
+  for (let i = 0; i < 9; i++) {
+    if (board[row][i] === num) return false;
+  }
+  for (let i = 0; i < 9; i++) {
+    if (board[i][col] === num) return false;
+  }
+  const startRow = Math.floor(row / 3) * 3;
+  const startCol = Math.floor(col / 3) * 3;
 
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (board[startRow + i][startCol + j] === num) return false;
+    }
+  }
 }
+isSolved();
 
 function prettyBoard() {
   /**
@@ -30,3 +54,4 @@ function prettyBoard() {
    * Подумай, как симпатичнее его вывести.
    */
 }
+console.log(board);
